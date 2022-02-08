fd24cfa8f0da41ab93b25633fe388f17623be332
Isa Hänninen <55230101+IsaEmilia@users.noreply.github.com>
2022-02-08 14:45:12 +0200

    Add code for user registration.
    Added code for user registration and validation, added user dashboard page.
    Minor changes to database.

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
 from flask import Flask, redirect, render_template, url_for, request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
+
 
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+app.config['SECRET_KEY'] = 'notimportant'
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
-
-app.secret_key = ''
 
 
 class Todo(db.Model):
@@ -21,18 +30,68 @@ class Todo(db.Model):
     def __repr__(self):
         return '<Task %r>' % self.id
 
-#login ?
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
 
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(
+        min=3, max=10,)], render_kw={"placeholder": "username"})
+
+    password = PasswordField(validators=[InputRequired(), Length(
+        min=5, max=20)], render_kw={"placeholder": "password"})    
+
+    submit = SubmitField("Register")
+
+    def validate_username(self, username):
+        existing_user_name = User.query.filter_by(
+            username=username.data).first()
+        if existing_user_name:
+            raise ValidationError(
+                "That name is in use."
+            )  
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(
+        min=3, max=10,)], render_kw={"placeholder": "username"})
+
+    password = PasswordField(validators=[InputRequired(), Length(
+        min=5, max=20)], render_kw={"placeholder": "password"})    
+
+    submit = SubmitField("Login")
+
+    def validate_username(self, username):
+        existing_user_name = User.query.filter_by(
+            username=username.data).first()
+        if existing_user_name:
+            raise ValidationError(
+                "That name is in use."
+            )    
+
+
+    
+
+# login routing and form shenanigans
 
 @app.route('/login', methods=['GET','POST'])
 def login():
-        return render_template('login.html')
+    form = LoginForm()
+    return render_template('login.html', form=form)
 
 
 
 @app.route('/register', methods=['GET','POST'])
 def register():
-        return render_template('register.html')
+    form = RegisterForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+
+    return render_template('register.html', form=form)
 
 
 
@@ -41,22 +100,6 @@ def logout():
     # remove the username from the session if it's there
     session.pop('username', None)
     return redirect(url_for('index'))
-
-
-    
-
-#login_manager = LoginManager()
-#login_manager.init_app(app)
-
-#@login_manager.user_loader
-#def load_user(user_id):
-#    return User.get(user_id)
-
-
-
-#class User(db.Document):
-#    name = db.StringField()
-
 
 
 
@@ -78,6 +121,7 @@ def index():
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
 
+
 # code for deleting entries
 @app.route('/delete/<int:id>')
 def delete(id):
@@ -97,20 +141,14 @@ def update(id):
     task = Todo.query.get_or_404(id)
     if request.method == 'POST':
         task.content = request.form['content']
-
         try:
             db.session.commit()
             return redirect('/')
-
         except:
-            return 'Nothing happened... Seems like I am not functioning properly.'    
-
+            return 'Nothing happened... Seems like I am not functioning properly.'   
 
     else:
         return render_template('update.html', task=task)
-
-
-
 
 
 
